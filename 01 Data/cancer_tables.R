b47a0b98f7e68d11011379571636e214d17a64c6
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-04-14 19:16:00 -0500

    Added R-script to load all tables... But, There are some issues! Tables greater than 1k rows seem to take VERY long to load. We might have to talk to Canatta about this.

---
+++ 01 Data/cancer_tables.R
@@ -0,0 +1,23 @@
+age_adj_rates_child.df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from AGE_ADJ_RATES_CHILD"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_dah3227', PASS='orcl_dah3227', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+age_adj_rates_iccc.df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from age_adj_rates_iccc"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_dah3227', PASS='orcl_dah3227', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+age_specific_rates_child <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from AGE_specific_RATE_CHILD"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_dah3227', PASS='orcl_dah3227', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+age_specific_rates_iccc <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from AGE_specific_RATES_iccc"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_dah3227', PASS='orcl_dah3227', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+brain_cancer_by_site <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from brain_cancer_by_site"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_dah3227', PASS='orcl_dah3227', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+#Cancer by age needs to be reuploaded!
+cancer_by_age <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from cancer_by_age"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_dah3227', PASS='orcl_dah3227', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+#This dataset is MASSIVE, and probably will only be used in Tableau.
+#Load time may be VERY long. 
+#cancer_by_area <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from cancer_by_area"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_dah3227', PASS='orcl_dah3227', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+#Also large, couldn't even load it!
+#cancer_by_site <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from cancer_by_site"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_dah3227', PASS='orcl_dah3227', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+child_cancer_by_site <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from child_cancer_by_site"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_dah3227', PASS='orcl_dah3227', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
+
+child_cancer_iccc <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from child_cancer_iccc"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521:ORCL', USER='C##cs329e_dah3227', PASS='orcl_dah3227', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
