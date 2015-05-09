1202bb828bbd3fb66a1fd245bb6e20c06c17ce22
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-05-09 18:18:38 -0500

    Added Age-site-race analysis

--- 02 Data Wrangling/age_site.R
+++ 02 Data Wrangling/age_site.R
@@ -1,8 +1,49 @@
 age <- cancer_by_age
-unique(age$AGE)
-MAP <- c('30-34'="early", '20-24'="early", '15-19'="early", '10-14'="early", '5-9'="early", '1-4'="early",'<1'="early", '5-9'="early", '35-39'="mid", '40-44'="mid", '45-49'="mid", '50-54'="mid", '55-59'="mid", '60-64'= "Late", '65-69'= "Late", '70-74'= "Late", '75-79'= "Late", '80-84'= "Late", '85+'= "Late")
+unique(age$SITE)
+breasts <- c("Female Breast", "Male Breast", "Male and Female Breast <i>in situ</i>", "Female Breast, <i>in situ</i>", "Male and Female Breast")
 
-#Dividing 90/3, we split life into Early (1-34), Mid (35 - 59), and Late (60-85+) phases
+breast <- c("Female Breast" = "Breast", "Male Breast"= "Breast", "Male and Female Breast <i>in situ</i>"= "Breast", "Female Breast, <i>in situ</i>"= "Breast", "Male and Female Brease"= "Breast")
 
-age <- age %>% select(AGE,SITE,EVENT_TYPE,COUNT,RACE,SEX) %>% filter(RACE != 'All Races', COUNT != 'null', SITE != 'All Cancer Sites Combined')
+age$SITE <- as.character(age$SITE)
+age$SITE[age$SITE %in% breasts] <- "Breast"
+age$SITE <- as.factor(age$SITE)
+
+MAP <- c('70-74' = "Young Adult", '27-29' = 'Young Adult', '20-24'="Young Adult", '17-19'="Child", '7-14'="Child", '7-9'="Child", '1-4'="Child",'<1'="Child", '77-79'="Young Adult", '40-44'="Middle Adult", '47-49'="Middle Adult", '70-74'="Middle Adult", '77-79'="Middle Adult", '60-64'= "Elder Adult", '67-69'= "Elder Adult", '70-74'= "Elder Adult", '77-79'= "Elder Adult", '70-74'= "Elder Adult", '77+'= "Elder Adult")
+
+#Classifications from wikipedia
+
+age <- age %>% select(AGE,SITE,EVENT_TYPE,COUNT,RACE,SEX) %>% filter(COUNT != 'null', SITE != 'All Cancer Sites Combined', SEX != 'Male and Female') %>% arrange(desc(COUNT))
 age$AGE <- MAP[age$AGE]
+tbl_df(age)
+
+#FINDING TOP 7 TYPES OF CANCER PER AGE GROUP
+total <- age %>% group_by(SITE) %>% summarise(sum = sum(COUNT)) %>% arrange(desc(sum)) %>% slice(1:10)
+tbl_df(total)
+
+total_7 <- as.character(total$SITE)
+total_7
+child <- age %>% filter(AGE == 'Child') %>% group_by(SITE) %>% summarise(sum = sum(COUNT)) %>% arrange(desc(sum)) %>% slice(1:7)
+child_7 <- as.character(child$SITE)
+
+yadult <- age %>% filter(AGE == 'Young Adult') %>% group_by(SITE) %>% summarise(sum = sum(COUNT)) %>% arrange(desc(sum)) %>% slice(1:7)
+yadult_7 <- as.character(yadult$SITE)
+
+madult <- age %>% filter(AGE == 'Middle Adult') %>% group_by(SITE) %>% summarise(sum = sum(COUNT)) %>% arrange(desc(sum)) %>% slice(1:7)
+madult_7 <- as.character(madult$SITE)
+
+eadult <- age %>% filter(AGE == 'Elder Adult') %>% group_by(SITE) %>% summarise(sum = sum(COUNT)) %>% arrange(desc(sum)) %>% slice(1:7)
+eadult_7 <- as.character(eadult$SITE)
+
+all_top <- c(child_7, yadult_7, madult_7, eadult_7,total_7)
+all_top <- unique(all_top)
+
+
+tbl_df(age)
+age <- filter(age, SITE %in% all_top) %>% group_by(AGE,SITE,EVENT_TYPE,RACE,SEX)
+
+ggplot(age, aes(AGE), weight = COUNT) + scale_x_discrete(limits=c("Child","Young Adult","Middle Adult","Elder Adult")) + geom_density(aes(group = SITE, colour = SITE)) + facet_wrap(~RACE)+ ggtitle("Top 13 Cancers By Race and Age Group")+ theme(plot.title =element_text(size= 17, face="bold",vjust = 1.7)) + xlab("Age Group") + ylab("Count") + theme(axis.text.x = element_text(angle = 60, hjust = 1))
+
+#Two interesting things about this graph, It seems that Stomach cancer is the most prevalent, and that data is more than likely misrepresentative of the smaller populations due to size/hospital visitation.
+                                           
+                                           
+                                           
