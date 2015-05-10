9cfb948ec999790852834f3a46c8e33a3bd2497c
ACollectionOfAtoms <hernandezdavid88@utexas.edu>
2015-05-10 15:46:22 -0500

    Changessssssss

--- 02 Data Wrangling/age_site.R
+++ 02 Data Wrangling/age_site.R
@@ -45,7 +45,7 @@ all_top <- unique(all_top)
 tbl_df(age)
 age <- filter(age, SITE %in% all_top) %>% group_by(AGE,SITE,EVENT_TYPE,RACE,SEX)
 
-ggplot(age, aes(AGE), weight = COUNT, fill=SITE) + scale_x_discrete(limits=c("Child","Young Adult","Middle Adult","Elder Adult")) + geom_density(aes(group = SITE, color=SITE)) + ggtitle("Top 13 Cancers Age Group")+ theme(plot.title =element_text(size= 17, face="bold",vjust = 1.7)) + xlab("Age Group") + ylab("Count") + theme(axis.text.x = element_text(angle = 60, hjust = 1)) 
+ggplot(age, aes(AGE), weight = COUNT, fill=SITE) + scale_x_discrete(limits=c("Child","Young Adult","Middle Adult","Elder Adult")) + geom_density(aes(group = SITE, color=SITE)) + ggtitle("Top 13 Cancers Age Group")+ theme(plot.title =element_text(size= 17, face="bold",vjust = 1.7)) + xlab("Age Group") + ylab("Density") + theme(axis.text.x = element_text(angle = 60, hjust = 1)) 
 
 #Joined data
                                            
